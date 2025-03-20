2eb08617a776f2553d3768c030021a6f2d3ca619
Bahry Lanckerz <bahrylanckerz@gmail.com>
2025-03-20 13:18:10 +0700

    crud ticket

--- app/Filament/Resources/TicketResource.php
+++ app/Filament/Resources/TicketResource.php
@@ -2,16 +2,28 @@
 
 namespace App\Filament\Resources;
 
-use App\Filament\Resources\TicketResource\Pages;
-use App\Filament\Resources\TicketResource\RelationManagers;
-use App\Models\Ticket;
 use Filament\Forms;
-use Filament\Forms\Form;
-use Filament\Resources\Resource;
 use Filament\Tables;
+use App\Models\Ticket;
+use Filament\Forms\Form;
 use Filament\Tables\Table;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Fieldset;
+use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\Textarea;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\TimePicker;
+use Filament\Tables\Filters\SelectFilter;
 use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Resources\TicketResource\Pages;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\TicketResource\RelationManagers;
 
 class TicketResource extends Resource
 {
@@ -23,7 +35,61 @@ class TicketResource extends Resource
     {
         return $form
             ->schema([
-                //
+                Fieldset::make('Details Information')
+                ->schema([
+                    TextInput::make('name')
+                        ->required()
+                        ->maxLength(255),
+                    Textarea::make('address')
+                        ->rows(3)
+                        ->required()
+                        ->maxLength(255),
+                    FileUpload::make('thumbnail')
+                        ->image()
+                        ->required(),
+                    Repeater::make('photos')
+                        ->relationship('photos')
+                        ->schema([
+                            FileUpload::make('photo')
+                                ->image()
+                                ->required(),
+                        ]),
+                ]),
+                Fieldset::make('Additional Information')
+                    ->schema([
+                        RichEditor::make('about')
+                            ->required(),
+                        TextInput::make('path_video')
+                            ->required()
+                            ->maxLength(255),
+                        TextInput::make('price')
+                            ->required()
+                            ->numeric()
+                            ->prefix('IDR'),
+                        Select::make('is_popular')
+                            ->required()
+                            ->options([
+                                true => 'Popular',
+                                false => 'Not Popular',
+                            ])
+                            ->label('Popular'),
+                        Select::make('category_id')
+                            ->preload()
+                            ->required()
+                            ->searchable()
+                            ->relationship('category', 'name'),
+                        Select::make('seller_id')
+                            ->preload()
+                            ->required()
+                            ->searchable()
+                            ->relationship('seller', 'name'),
+                        TimePicker::make('open_time_at')
+                            ->required()
+                            ->label('Open Time'),
+                        TimePicker::make('closed_time_at')
+                            ->required()
+                            ->label('Closed Time'),
+                    ]),
             ]);
     }
 
@@ -31,12 +97,26 @@ class TicketResource extends Resource
     {
         return $table
             ->columns([
-                //
+                ImageColumn::make('thumbnail'),
+                TextColumn::make('name')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('category.name')
+                    ->label('Category'),
+                IconColumn::make('is_popular')
+                    ->boolean()
+                    ->trueColor('success')
+                    ->falseColor('danger')
+                    ->trueIcon('heroicon-o-check-circle')
+                    ->falseIcon('heroicon-o-x-circle')
+                    ->label('Popular'),
             ])
             ->filters([
-                //
+                SelectFilter::make('category_id')
+                    ->relationship('category', 'name'),
             ])
             ->actions([
+                Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
             ])
             ->bulkActions([
